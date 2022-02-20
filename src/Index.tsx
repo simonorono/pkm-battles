8f788dbbda46b5a6c205b09c12c5b271b314ded6
Simón Oroño <simonorono@protonmail.com>
2022-02-19 23:00:08 -0400

    Create folder-like index page

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,11 +1,18 @@
 import dates from './battles/index.json'
+import { FolderIcon } from '@heroicons/react/solid'
+import { Link } from 'react-router-dom'
 
 export default function Index() {
   return (
     <div className="mx-auto max-w-7xl">
-      <ul>
+      <ul className="grid grid-cols-4 gap-5">
         {dates.map(date => (
-          <li key={date}>{date}</li>
+          <li key={date} className="rounded-lg border">
+            <Link to={`/${date}`} className="inline-flex h-full w-full items-center">
+              <FolderIcon className="w-20 text-emerald-700" />
+              <h3>{date}</h3>
+            </Link>
+          </li>
         ))}
       </ul>
     </div>
