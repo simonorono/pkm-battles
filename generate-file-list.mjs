ac009dd7c44df222d64e47fcd9dca865cc80de63
Simón Oroño <simonorono@protonmail.com>
2022-02-19 21:01:31 -0400

    Create script to generate data files from the battle data

---
+++ generate-file-list.mjs
@@ -0,0 +1,41 @@
+import fs from 'fs'
+
+const BATTLES_DIR = './battles'
+const TARGET_DIR = './src/battles'
+
+const files = fs.readdirSync(BATTLES_DIR)
+
+let result = {}
+
+for (const file of files) {
+  const parts = file.replaceAll('.html', '').split('-')
+
+  const format = parts[0]
+  const date = `${parts[1]}-${parts[2]}-${parts[3]}`
+  const player1 = parts[4]
+  const player2 = parts[5]
+
+  const object = { format, date, player1, player2, file }
+
+  if (result[date]) {
+    result[date].push(object)
+  } else {
+    result[date] = [object]
+  }
+}
+
+fs.rmSync('./src/battles', { recursive: true })
+
+fs.mkdirSync('./src/battles', { recursive: true })
+
+for (const date of Object.keys(result)) {
+  fs.writeFileSync(`${TARGET_DIR}/${date}.json`, JSON.stringify(result[date]), { flag: 'w+' })
+}
+
+fs.writeFileSync(
+  `${TARGET_DIR}/index.json`,
+  JSON.stringify(
+    Object.keys(result).sort((a, b) => a > b ? -1 : 1)
+  ),
+  { flag: 'w+' }
+)
