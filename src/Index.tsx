2d5d3c19d68dfb7a35576d6590b597209f7f90f4
Simón Oroño <simonorono@protonmail.com>
2022-02-20 20:32:31 -0400

    Make index page responsive

--- src/Index.tsx
+++ src/Index.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export default function Index() {
   return (
     <div className="mx-auto max-w-7xl">
-      <ul className="grid grid-cols-4 gap-5">
+      <ul className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {dates.map(date => (
           <li key={date} className="rounded-lg border">
             <Link to={`/${date}`} className="inline-flex h-full w-full items-center">
