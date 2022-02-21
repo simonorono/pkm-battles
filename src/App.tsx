fe2b97733d107f873d80b66341f98a0ea7d99140
Simón Oroño <simonorono@protonmail.com>
2022-02-20 20:33:59 -0400

    Make header responsive

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <div className="flex min-h-screen flex-col px-2">
         <header className="mx-auto w-full max-w-7xl">
-          <div className="mt-8 mb-5 flex items-baseline justify-between border-b-2 border-black pb-2">
+          <div className="mt-8 mb-5 flex flex-col items-baseline justify-between border-b-2 border-black pb-2 sm:flex-row">
             <h1 className="font-serif text-3xl">Pokémon Battle History</h1>
             <h2>Simón Oroño</h2>
           </div>
