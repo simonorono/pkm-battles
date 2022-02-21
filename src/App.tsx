23ca70ff1ea462249eabb488ddf3b1bc4a19bf7c
Simón Oroño <simonorono@protonmail.com>
2022-02-20 21:14:55 -0400

    Make the H1 a link to the index

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Battle from './Battle'
 import Battles from './Battles'
 import Index from './Index'
@@ -9,7 +9,9 @@ function App() {
       <div className="flex min-h-screen flex-col px-2">
         <header className="mx-auto w-full max-w-7xl">
           <div className="mt-8 mb-5 flex flex-col items-baseline justify-between border-b-2 border-black pb-2 sm:flex-row">
-            <h1 className="font-serif text-3xl">Pokémon Battle History</h1>
+            <h1 className="font-serif text-3xl hover:underline">
+              <Link to="/">Pokémon Battle History</Link>
+            </h1>
             <h2>Simón Oroño</h2>
           </div>
         </header>
