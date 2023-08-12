0e35e961362bc62a6f68a486b980ab186c175ed5
Simón Oroño <simonorono@protonmail.com>
2023-08-12 10:52:07 -0400

    Add copyleft notice

--- src/Index.tsx
+++ src/Index.tsx
@@ -4,6 +4,9 @@ export default function Index() {
   return (
     <div className="mx-auto max-w-7xl">
       <BattleTable />
+      <p className="py-4 text-center">
+        © Simón Oroño
+      </p>
       <div className="h-32" />
     </div>
   )
