0bf8ea9ec3f6ee37046a2cf808ebd3af498a7e51
Simón Oroño <simonorono@protonmail.com>
2023-08-13 09:05:55 -0400

    Add notice for small screens

--- src/Index.tsx
+++ src/Index.tsx
@@ -3,6 +3,13 @@ import BattleTable from './components/BattleTable'
 export default function Index() {
   return (
     <div className="mx-auto max-w-7xl">
+      <div className="border-2 border-red-900 bg-red-100 md:hidden">
+        <p className="p-6 text-red-900">
+          Replays stored in this site come directly from «Pokémon Showdown!» and
+          will not look good on mobile devices.
+        </p>
+      </div>
+
       <BattleTable />
     </div>
   )
