61cc98c2301a345df66c721cac4f121449fc983d
Simón Oroño <simonorono@protonmail.com>
2023-08-13 08:16:55 -0400

    Add some padding on mobile

--- src/components/BattleTable.tsx
+++ src/components/BattleTable.tsx
@@ -14,9 +14,9 @@ export default function BattleTable() {
 
   return (
     <div className="mt-8 flow-root">
-      <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div className="mx-4 my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-          <div className="overflow-hidden border sm:rounded-lg">
+          <div className="overflow-hidden rounded-lg border">
             <table className="min-w-full divide-y divide-gray-300">
               <TableHeader />
               <tbody className="divide-y divide-gray-200 bg-white">
